2638f523c3656a551f4dcc93103ec0e6ba382ba2
viktoriazaharova <zaharova181114@gmail.com>
2020-08-27 08:31:34 +0300

    add block

--- app/js/common.js
+++ app/js/common.js
@@ -1,3 +1,47 @@
-$(document).ready(function() {
-    console.log( "ready!" );
+$('.main-header-slider').slick({
+    slidesToShow: 1,
+    arrows: false,
+    fade: true,
+    autoplay: true,
+    autoplaySpeed: 3000,
+});
+
+$('.main-command-slider').slick({
+    slidesToShow: 3,
+    slidesToScroll: 1,
+    dots: true,
+    prevArrow: '<button type="button" class="slick-prev"></button>',
+    nextArrow: '<button type="button" class="slick-next"></button>',
+    appendDots: '.main-command-slider__nav',
+    appendArrows: '.main-command-slider__nav',
+    responsive: [
+        {
+            breakpoint: 800,
+            settings: {
+                slidesToShow: 2,
+                slidesToScroll: 1,
+            }
+        },
+        {
+            breakpoint: 550,
+            settings: {
+                slidesToShow: 2,
+                slidesToScroll: 1,
+                variableWidth: true,
+                arrows: false,
+                autoplay: true,
+                autoplaySpeed: 3000,
+            }
+        }
+    ]
+});
+
+$('.btn-burger').click(function () {
+    $('.overlay').fadeIn();
+    $('.mobile-menu').fadeIn();
+});
+
+$('.btn-close').click(function () {
+    $('.mobile-menu').fadeOut();
+    $('.overlay').fadeOut();
 });
