8e0e93d1d66d3c9d4ad78f54571b9a5dea4d2a8e
viktoriazaharova <zaharova181114@gmail.com>
2020-08-30 11:01:26 +0300

    add media

--- app/js/common.js
+++ app/js/common.js
@@ -1,11 +1,3 @@
-// $('.main-header-slider').slick({
-//     slidesToShow: 1,
-//     arrows: false,
-//     fade: true,
-//     autoplay: true,
-//     autoplaySpeed: 3000,
-// });
-
 $(document).ready(function() {
     $('.main-header-slider .main-header-slider__bg').eq(0).addClass('active').fadeIn(1000);
     $('.logo-slider .logo-slider__img').eq(0).addClass('active').fadeIn(1000);
@@ -40,28 +32,28 @@ function blockAnimate() {
     });
 };
 
-
-$(document).scroll(function () {
-    if ($(this).scrollTop() <= ($(".main-header-top").offset().top - $(window).height())) {
-        $('.main-top-navigation').addClass('fixed');
-    } else {
-        $('.main-top-navigation').removeClass('fixed');
-    }
-});
-
-$(function(){
-    $nav = $('.main-top-navigation');
-    // $nav.css('width', $nav.outerWidth());
-    $window = $(window);
-    $h = $nav.offset().top;
-    $window.scroll(function(){
-        if ($window.scrollTop() > $h) {
-            $nav.addClass('fixed');
-        } else {
-            $nav.removeClass('fixed');
-        }
-    });
-});
+// включить на странице, где есть этот блок
+// $(document).scroll(function () {
+//     if ($(this).scrollTop() <= ($(".main-header-top").offset().top - $(window).height())) {
+//         $('.main-top-navigation').addClass('fixed');
+//     } else {
+//         $('.main-top-navigation').removeClass('fixed');
+//     }
+// });
+//
+// $(function(){
+//     $nav = $('.main-top-navigation');
+//     $window = $(window);
+//     $h = $nav.offset().top;
+//     $window.scroll(function(){
+//         if ($window.scrollTop() > $h) {
+//             $nav.addClass('fixed');
+//         } else {
+//             $nav.removeClass('fixed');
+//         }
+//     });
+// });
+// включить на странице, где есть этот блок
 
 $('.main-command-slider').slick({
     slidesToShow: 3,
@@ -86,6 +78,39 @@ $('.main-command-slider').slick({
                 slidesToScroll: 1,
                 variableWidth: true,
                 arrows: false,
+                autoplay: true,
+                autoplaySpeed: 3000,
+            }
+        }
+    ]
+});
+
+$('.users-service-slider').slick({
+    slidesToShow: 2,
+    slidesToScroll: 1,
+    dots: true,
+    variableWidth: true,
+    prevArrow: '<button type="button" class="slick-prev"></button>',
+    nextArrow: '<button type="button" class="slick-next"></button>',
+    appendDots: '.users-service-slider__nav',
+    appendArrows: '.users-service-slider__nav',
+    responsive: [
+        {
+            breakpoint: 800,
+            settings: {
+                slidesToShow: 2,
+                slidesToScroll: 1,
+                variableWidth: true,
+            }
+        },
+        {
+            breakpoint: 550,
+            settings: {
+                slidesToShow: 2,
+                slidesToScroll: 1,
+                variableWidth: true,
+                arrows: false,
+                dots: false,
                 autoplay: true,
                 autoplaySpeed: 3000,
             }
